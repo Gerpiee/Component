b0d5a21909118b94f56594b11511915bd55e9d11
garip <garip.aksoy.123@gmail.com>
2022-03-27 22:56:16 +0300

    component yapısı
    component yapısı mantığı kavrandı oluşturuldu ve ekrana birden fazla yazdırıldı

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import User from "./components/User.js"; 
+import './App.css';
+
+
+function App() {
+    const test = 34;
+    const isAuth=false;
+  return (
+    
+    <div className="App">
+     <h4>My Component</h4>
+     <User/>
+     <User/>
+     <User/>
+     <User/>
+
+
+    </div>
+  
+  );
+}
+
+export default App;
